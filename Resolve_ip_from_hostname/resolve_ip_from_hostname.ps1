3095e6da6b15850051eff6330df251a6df690559
gianniskt <gianniskt@gmail.com>
2021-04-20 11:47:38 +0300

    Upload Powershell Resolve_ip_from_hostname

---
+++ Resolve_ip_from_hostname/resolve_ip_from_hostname.ps1
@@ -0,0 +1,29 @@
+﻿#Insert the hostnames in "hostnames.txt"
+#Prerequisites -> Set-ExecutionPolicy UnRestricted -File .\resolve_ip_from_hostname.ps1
+#author: Koutroumpis Giannis
+
+Get-Content .\hostnames.txt | ForEach {
+
+    $details = Test-Connection -ComputerName $_ -Count 1 -ErrorAction SilentlyContinue
+
+    if ($details) {
+
+        $props = @{
+            ComputerName = $_
+            IP = $details.IPV4Address.IPAddressToString
+        }
+
+        New-Object PsObject -Property $props
+    }
+
+    Else {    
+        $props = @{
+            ComputerName = $_
+            IP = 'Unreachable'
+        }
+
+        New-Object PsObject -Property $props
+    }
+
+} | Sort ComputerName | Export-Csv .\hosts.csv -Delimiter ';'  -NoTypeInformation
+    set-content .\hosts.csv ((get-content .\hosts.csv) -replace '"')
